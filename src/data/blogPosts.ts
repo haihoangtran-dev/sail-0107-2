6dafd8acd1d2095a5d52284ab7bb3d6d2000dcde
haihoangtran-dev <haihoangtran.dev@gmail.com>
2025-07-08 22:53:46 +0700

    Updated blogPosts.ts

--- src/data/blogPosts.ts
+++ src/data/blogPosts.ts
@@ -1,10 +1,26 @@
 import { BlogPost } from '../types';
 import { 
   quyTrinhThanhLapLLCTaiHoaKy,
-  huongDanDangKyLLCTaiDelaware
+  huongDanDangKyLLCTaiDelaware,
+  llcLuaChonLyTuongChoKinhDoanhOnlineTaiMy
 } from './blogContent';
 
 export const blogPosts: BlogPost[] = [
+        {
+    id: 3,
+    title: 'LLC: Lựa Chọn Lý Tưởng Cho Kinh Doanh Online Tại Mỹ',
+    slug: 'lc-lua-chon-ly-tuong-cho-kinh-doanh-online-tai-my',
+    excerpt:
+      'Khi bạn bắt đầu một doanh nghiệp tại Mỹ, việc chọn đúng loại hình doanh nghiệp Mỹ là yếu tố quan trọng để đảm bảo sự thành công lâu dài.',
+    content: llcLuaChonLyTuongChoKinhDoanhOnlineTaiMy,
+    author: 'Sail Agency',
+    publishDate: '2025-04-10',
+    category: 'LLC tại Mỹ',
+    tags: ['LLC', 'Đăng ký doanh nghiệp'],
+    readTime: '15 phút đọc',
+    image:
+      'https://images.pexels.com/photos/590022/pexels-photo-590022.jpeg?auto=compress&cs=tinysrgb&w=800',
+  },
       {
     id: 1,
     title: 'Quy trình thành lập LLC tại Hoa Kỳ: Hướng dẫn chi tiết cho doanh nghiệp quốc tế',
